b33cce531e9e89a291ba778a84934d25865099f2
Lucas Souto <lucasinfologos@gmail.com>
2018-07-23 16:08:35 -0300

    Add header and component Search

---
+++ src/components/layout/AppContent.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Olá</h1>
+    </div>
+</template>
---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="info">
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar-brand href="#">Twitter</b-navbar-brand>
+    <b-navbar-nav class="ml-auto">
+      <search></search>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+import Search from '@/components/Search.vue'
+
+export default {
+  name: 'Header',
+  components:{
+    Search
+  }
+}
+</script>
