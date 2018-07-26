054ee7f52ee2e31b74dd304a9d2b9066d5b3a665
Lucas Souto <lucasinfologos@gmail.com>
2018-07-26 16:08:39 -0300

    Add fetchTwitter

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,12 +1,39 @@
 <template>
   <b-nav-form>
-    <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-    <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-  </b-nav-form>    
+    <b-form-input
+      v-model="query"
+      size="sm"
+      class="mr-sm-2"
+      type="text"
+      placeholder="Search"/>
+
+    <b-button
+      @click="fetchTwitter"
+      size="sm"
+      class="my-2 my-sm-0"
+      >Search</b-button>
+  </b-nav-form>
 </template>
 
 <script>
+import { getSearch } from '@/services/twitter'
+
 export default {
-  name: 'Search'  
+  name: 'Search',
+  data () {
+    return {
+      query: ''
+    }
+  },
+  methods:{
+  fetchTwitter(){
+    getSearch(encodeURIComponent(this.query))
+      .then(data => {
+        this.$store.dispatch('resultsSearch', data)
+        this.$store.dispatch('setTitleSearch', this.query)
+    })
+  }
+}
+
 }
 </script>
