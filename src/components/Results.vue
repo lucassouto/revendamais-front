c490e7528734a319cbf96aaffa4106aac81b2add
Lucas Souto <lucasinfologos@gmail.com>
2018-07-30 13:51:20 -0300

    Disable immediate check scroll

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,5 +1,10 @@
 <template>
-<div v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="20">
+<div
+  v-infinite-scroll="loadMore"
+  infinite-scroll-disabled="busy"
+  infinite-scroll-distance="20"
+  infinite-scroll-immediate-check="flase">
+
   <h3>{{ getTitleSearch }}</h3>
   <b-list-group v-for="(x, index) in resultSearch" v-bind:key="index">
       <b-list-group-item v-for="(item, index) in x.statuses" v-bind:key="index">
