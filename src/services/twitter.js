4bb6424bbd6b929467b26b3ded8ee249b7a169f2
Lucas Souto <lucasinfologos@gmail.com>
2018-07-30 13:47:28 -0300

    Refactor routes

--- src/services/twitter.js
+++ src/services/twitter.js
@@ -1,8 +1,8 @@
 import Http from './http'
 
-export const getTrends = (woeid = '') => {
-  if (woeid !== '') {
-    return Http.get(`/trends/${woeid}/`)
+export const getTrends = (location = '') => {
+  if (location !== '') {
+    return Http.get(`trends/${location}/`)
       .then(response => response.data)
   } else {
     return Http.get(`/trends/`)
@@ -11,21 +11,21 @@ export const getTrends = (woeid = '') => {
 }
 
 export const getLocationsTrends = () => {
-  return Http.get(`/locations/`)
+  return Http.get(`locations/`)
     .then(response => response.data)
 }
 
 export const getLastResearchs = () => {
-  return Http.get('/latestsearches/')
+  return Http.get('latestsearches/')
     .then(response => response.data)
 }
 
 export const getSearch = term => {
-  return Http.get(`/search/${term}/`)
+  return Http.get(`search/${term}/`)
     .then(response => response.data)
 }
 
 export const getSearchNext = query => {
-  return Http.get(`/search-raw-query/${query}/`)
+  return Http.get(`search-raw-query/${query}/`)
     .then(response => response.data)
 }
